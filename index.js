29efa8efb6b792462bc11905136a554cda35e38e
lanetteamakshay <lanetteam.akshay@gmail.com>
2017-08-03 11:11:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Developer39 on 8/3/2017.
+ */
+
+
+const express=require('express');
+
+const app=express();
+
+app.get('/' ,function (req,res) {
+    res.send({hi:'there'});
+});
+
+
+const PORT=process.env.PORT || 5000 ;
+app.listen(PORT);
